35723a2ce42b9ea32081a79cc730dbb0d9925f7a
astwys <astwys@gmail.com>
2014-07-03 16:09:20 +0200

    finally deleted exit-case

--- Cases.java
+++ Cases.java
@@ -35,7 +35,6 @@ public class Cases
                 case "gotocastle": CasePrintouts.goToCastle(view); break;
                 case "gotoschool": CasePrintouts.goToSchool(view); break;
                 case "gotobarracks": CasePrintouts.goToBarracks(view); break;
-                case "exit": CasePrintouts.exit(view); break;
                 case "info": CasePrintouts.info(view); break;
                 case "electking": CasePrintouts.electking(view); break;
                 default: view.addText(choice + " is not a command\n"); break;
